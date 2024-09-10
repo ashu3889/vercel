fba61a66ead6336414e0f6d8f13473a3f341e73c
Your Name <[ashutosh.singh3889@gmail.com]>
2024-09-10 18:20:20 +0530

    Initial commit

--- src/components/Home/MainView.js
+++ src/components/Home/MainView.js
@@ -67,7 +67,7 @@ const mapDispatchToProps = dispatch => ({
 
 const MainView = props => {
   return (
-    <div className="col-md-12 col-sm-12">
+    <div className="col-md-12 col-sm-12 col-xs-12">
 
 
       <ArticleList
