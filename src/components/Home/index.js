6a886098bad4be161285ff88c00355e675705067
Your Name <[ashutosh.singh3889@gmail.com]>
2024-09-10 20:14:02 +0530

    Initial commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,6 @@
 import Banner from './Banner';
+import Footer from './Footer';
+
 import MainView from './MainView';
 import React from 'react';
 import Tags from './Tags';
@@ -52,21 +54,8 @@ class Home extends React.Component {
             <MainView />
           </div>
         </div>
-{/* 
-          <div class="footer" style= {{
-              position: 'fixed',
-              left: 0,
-              bottom: 0,
-              width: '100%',
-              color: 'white',
-              backgroundColor: 'grey',
-              color: 'black'
-           }}>
-          <p>
-          <b>Disclaimer:</b> I would like to remind you that the data contained in this website is not necessarily real-time nor accurate. All CFDs (stocks, indexes, futures), cryptocurrencies, and Forex prices are not provided by exchanges but rather by market makers, and so prices may not be accurate and may differ from the actual market price, meaning prices are indicative and not appropriate for trading purposes. Therefore I doesn't bear any responsibility for any trading losses you might incur as a result of using this data.
-            I will not accept any liability for loss or damage as a result of reliance on the information including data, quotes, charts and buy/sell signals contained within this website. Please be fully informed regarding the risks and costs associated with trading the financial markets, it is one of the riskiest investment forms possible.
-          </p>
-        </div> */}
+
+        <Footer/>
       </div>
     );
   }
