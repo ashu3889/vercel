079df18f389b130b610e35a375d6b5a0558a3559
Your Name <[ashutosh.singh3889@gmail.com]>
2024-09-15 10:15:22 +0530

    Initial commit

---
+++ src/components/ultils.js
@@ -0,0 +1,17 @@
+var b = a.filter(x => {
+    // let month = new Date(x.date).getMonth();
+    let year = new Date(x.date).getFullYear();
+    // ['NYSE', 'NASDAQ', 'NSE', 'BSE', 'coindcx'] 
+    if(year === 2024 ) {
+        return true;
+    }
+    return false;
+});
+
+var names = a.map(x => x.exchange);
+var uniq = names.reduce(function(a,b){
+    if (a.indexOf(b) < 0 ) a.push(b);
+    return a;
+  },[]);
+
+console.log(uniq, names);
